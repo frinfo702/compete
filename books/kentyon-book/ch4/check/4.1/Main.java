13b8746767bd96698ae6c2ed23cb2964e5826050
frinfo702 <121755762+frinfo702@users.noreply.github.com>
2026-03-13 20:49:33 +0900

    uyu

---
+++ books/kentyon-book/ch4/check/4.1/Main.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+
+public class Main {
+
+    static class Solution {
+
+        int[] memo = new int[50];
+
+        Solution() {
+            Arrays.fill(memo, -1);
+        }
+
+        public int tribonacci(int N) {
+            if (N == 0 || N == 1) {
+                return 0;
+            } else if (N == 2) {
+                return 1;
+            }
+
+            if (memo[N] != -1) {
+                return memo[N];
+            }
+
+            int result =
+                tribonacci(N - 1) + tribonacci(N - 2) + tribonacci(N - 3);
+            System.out.println(result);
+            return memo[N] = result;
+        }
+    }
+
+    static class TestCase {
+
+        int n;
+        int expected;
+
+        TestCase(int n, int expected) {
+            this.n = n;
+            this.expected = expected;
+        }
+    }
+
+    static void runTests() {
+        Solution solution = new Solution();
+        TestCase[] testCases = {
+            new TestCase(3, 1),
+            new TestCase(4, 2),
+            new TestCase(9, 44),
+        };
+
+        for (TestCase test : testCases) {
+            int actual = solution.tribonacci(test.n);
+            assert (actual == test.expected);
+        }
+    }
+
+    public static void main(String[] args) {
+        runTests();
+        System.out.println("ok");
+    }
+}
